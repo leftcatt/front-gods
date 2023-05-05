3b870930cf9d47ce367f3abaeb69cb6dc35b7a16
leftcatt <leftcat9@gmail.com>
2023-05-05 20:45:02 +0400

    created table app

---
+++ table/src/pages/data.js
@@ -0,0 +1,65 @@
+export const data =[
+  {
+    "name": "Nguyen",
+    "surnamme": "Louisa ",
+    "email": "louisanguyen@ozean.com",
+    "phone": "+1 (973) 516-3228",
+    "address": "247 Dahill Road, Marysville, Massachusetts, 3737"
+  },
+  {
+    "name": "Castro",
+    "surnamme": "Katy ",
+    "email": "katycastro@ozean.com",
+    "phone": "+1 (886) 521-2969",
+    "address": "226 Dinsmore Place, Nutrioso, Federated States Of Micronesia, 1697"
+  },
+  {
+    "name": "Holman",
+    "surnamme": "Wiggins ",
+    "email": "wigginsholman@ozean.com",
+    "phone": "+1 (872) 478-3898",
+    "address": "155 Fane Court, Limestone, Iowa, 9636"
+  },
+  {
+    "name": "Dominguez",
+    "surnamme": "Skinner ",
+    "email": "skinnerdominguez@ozean.com",
+    "phone": "+1 (821) 559-2809",
+    "address": "942 John Street, Bodega, Marshall Islands, 9622"
+  },
+  {
+    "name": "Mckinney",
+    "surnamme": "Donovan ",
+    "email": "donovanmckinney@ozean.com",
+    "phone": "+1 (925) 586-2706",
+    "address": "642 Turner Place, Kula, Florida, 7659"
+  },
+  {
+    "name": "Logan",
+    "surnamme": "Sheila ",
+    "email": "sheilalogan@ozean.com",
+    "phone": "+1 (907) 498-2062",
+    "address": "607 Bennet Court, Mayfair, Vermont, 3300"
+  },
+  {
+    "name": "Wright",
+    "surnamme": "Bird ",
+    "email": "birdwright@ozean.com",
+    "phone": "+1 (911) 558-3737",
+    "address": "298 Juliana Place, Rivers, Kansas, 1101"
+  },
+  {
+    "name": "Lambert",
+    "surnamme": "Saundra ",
+    "email": "saundralambert@ozean.com",
+    "phone": "+1 (845) 575-2676",
+    "address": "892 Ovington Avenue, Enlow, Ohio, 8578"
+  },
+  {
+    "name": "Gibson",
+    "surnamme": "Dora ",
+    "email": "doragibson@ozean.com",
+    "phone": "+1 (982) 430-2879",
+    "address": "341 Martense Street, Islandia, Arkansas, 4392"
+  }
+]
